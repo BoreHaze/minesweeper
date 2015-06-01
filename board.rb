c088f2a395aaf1c13c6690b5bdf99daa94462fd8
Maxfield Lewin <mslewin@gmail.com>
2015-06-01 16:13:37 -0400

    Refactor and move board functionality into Board class

--- board.rb
+++ board.rb
@@ -13,11 +13,13 @@ class Board
     [ 1,  1]
   ]
 
-  attr_reader :number_of_mines
+  attr_reader :number_of_mines, :flagged_coords, :revealed_coords
 
   def initialize(number_of_mines)
     @number_of_mines = number_of_mines
     @rows = Array.new(BOARD_SIZE) {Array.new(BOARD_SIZE)}
+    @flagged_coords  = []
+    @revealed_coords = []
   end
 
   def set_mines
@@ -72,6 +74,92 @@ class Board
 
   def []=(x, y, tile)
     @rows[x][y] = tile
+  end
+
+  def draw_board
+    puts "   0 1 2 3 4 5 6 7 8" #should be parameterized
+    horizontal_line = " +-#{'-'*BOARD_SIZE*2}+"
+    0.upto(BOARD_SIZE - 1) do |row_idx|
+      puts horizontal_line
+      print "#{row_idx}| "
+      0.upto(BOARD_SIZE - 1) do |col_idx|
+
+        tile = self[row_idx, col_idx]
+
+        if @revealed_coords.include?([row_idx, col_idx])
+
+          if tile.is_mine?
+           print "* "
+          else
+           print "#{tile.num_neighbors} "
+          end
+
+        elsif @flagged_coords.include?([row_idx, col_idx])
+         print "F "
+        else
+         print "- "
+        end
+
+      end
+      puts "|"
+    end
+    puts horizontal_line
+  end
+
+  def won?
+    @revealed_coords.uniq.count == (BOARD_SIZE**2) - @number_of_mines
+  end
+
+  def lost?
+    return false if @revealed_coords.empty?
+    self[*(@revealed_coords.last)].is_mine?
+  end
+
+  def reveal(coord)
+    if self[*coord].num_neighbors == 0
+      explore_coords(coord)
+    else
+      @revealed_coords << coord
+    end
+  end
+
+  def get_neighbors(coord)
+    result = []
+    DELTAS.each do |d_x, d_y|
+      check_coord = [coord.first + d_x, coord.last + d_y]
+      next if @revealed_coords.include?(check_coord)
+      result << check_coord if in_bounds?(check_coord)
+    end
+    result
+  end
+
+  def explore_coords(coord)
+    queue = [coord]
+    until queue.empty?
+      next_coord = queue.shift
+      @revealed_coords << next_coord
+      #debugger
+      blank_neighbors = get_neighbors(next_coord)
+      blank_neighbors.each do |explore_coord|
+        if self[*explore_coord].num_neighbors == 0
+          queue << explore_coord
+        else
+          @revealed_coords << explore_coord
+        end
+      end
+    end
+  end
+
+  def update(move, coord)
+    if move == "r"
+      reveal(coord)
+    else
+      if @flagged_coords.include?(coord)
+        @flagged_coords.delete(coord)
+      else
+        @flagged_coords << coord
+      end
+    end
   end
 
 end
--- minesweeper.rb
+++ minesweeper.rb
@@ -8,63 +8,22 @@ class MineSweeper
     @board = Board.new(number_of_mines)
     @board.set_mines
     @board.set_numbers
-
-    @flagged_coords  = []
-    @revealed_coords = []
   end
 
   def play
 
     loop do
-      draw_board
+      @board.draw_board
       move = get_move
       coord = get_coordinate(move)
-      if move == "r"
-        reveal(coord)
-      else
-        if @flagged_coords.include?(coord)
-          @flagged_coords.delete(coord)
-        else
-          @flagged_coords << coord
-        end
-      end
-      break if lost? || won?
+      @board.update(move, coord)
+      break if @board.lost? || @board.won?
 
     end
-    draw_board
-    puts "You lost!" if lost?
-    puts "You win!" if won?
+    @board.draw_board
+    puts "You lost!" if @board.lost?
+    puts "You win!" if @board.won?
 
-  end
-
-  def draw_board
-    puts "   0 1 2 3 4 5 6 7 8" #should be parameterized
-    horizontal_line = " +-#{'-'*Board::BOARD_SIZE*2}+"
-    0.upto(Board::BOARD_SIZE - 1) do |row_idx|
-      puts horizontal_line
-      print "#{row_idx}| "
-      0.upto(Board::BOARD_SIZE - 1) do |col_idx|
-
-        tile = @board[row_idx, col_idx]
-
-        if @revealed_coords.include?([row_idx, col_idx])
-
-          if tile.is_mine?
-           print "* "
-          else
-           print "#{tile.num_neighbors} "
-          end
-
-        elsif @flagged_coords.include?([row_idx, col_idx])
-         print "F "
-        else
-         print "- "
-        end
-
-      end
-      puts "|"
-    end
-    puts horizontal_line
   end
 
   def get_move
@@ -86,61 +45,17 @@ class MineSweeper
         coordinate = gets.chomp.split(",").map(&:to_i)
         #debugger
         if move_type == "r"
-          valid_move = !@revealed_coords.include?(coordinate) &&
+          valid_move = !@board.revealed_coords.include?(coordinate) &&
             @board.in_bounds?(coordinate) &&
-            !@flagged_coords.include?(coordinate)
+            !@board.flagged_coords.include?(coordinate)
         else
-          valid_move = !@revealed_coords.include?(coordinate) &&
+          valid_move = !@board.revealed_coords.include?(coordinate) &&
             @board.in_bounds?(coordinate)
         end
         puts "Not a valid coordinate, try again." unless valid_move
       end
 
     coordinate
-  end
-
-  def won?
-    @revealed_coords.uniq.count == (Board::BOARD_SIZE**2) - @board.number_of_mines
-  end
-
-  def lost?
-    return false if @revealed_coords.empty?
-    @board[*(@revealed_coords.last)].is_mine?
-  end
-
-  def reveal(coord)
-    if @board[*coord].num_neighbors == 0
-      explore_coords(coord)
-    else
-      @revealed_coords << coord
-    end
-  end
-
-  def get_neighbors(coord)
-    result = []
-    Board::DELTAS.each do |d_x, d_y|
-      check_coord = [coord.first + d_x, coord.last + d_y]
-      next if @revealed_coords.include?(check_coord)
-      result << check_coord if @board.in_bounds?(check_coord)
-    end
-    result
-  end
-
-  def explore_coords(coord)
-    queue = [coord]
-    until queue.empty?
-      next_coord = queue.shift
-      @revealed_coords << next_coord
-      #debugger
-      blank_neighbors = get_neighbors(next_coord)
-      blank_neighbors.each do |explore_coord|
-        if @board[*explore_coord].num_neighbors == 0
-          queue << explore_coord
-        else
-          @revealed_coords << explore_coord
-        end
-      end
-    end
   end
 
 end
