4084884e12f865f17740893b0c73de6591782ca6
Maxfield Lewin <mslewin@gmail.com>
2015-06-01 11:21:08 -0400

    Create set_mines for board

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require './tile.rb'
+
 class Board
   BOARD_SIZE = 9
 
@@ -7,7 +9,21 @@ class Board
   end
 
   def set_mines
+    @number_of_mines.times do
 
+      placed_mine = false
+
+      until placed_mine
+        x = rand(BOARD_SIZE - 1)
+        y = rand(BOARD_SIZE - 1)
+
+        if self[x, y].nil?
+          self[x, y] = Tile.new(true)
+          placed_mine = true
+        end
+      end
+
+    end
   end
 
   def [](x, y)
